372f499e4bab83d58ed4531c8fd58e0ddf46508d
ezio008 <lorenzoberzosa94@gmail.com>
2023-04-19 14:33:33 +0200

    Fix multiple messages

--- WhatsappNet.Api/Services/WhatsappCloud/WhasappCloudService.cs
+++ WhatsappNet.Api/Services/WhatsappCloud/WhasappCloudService.cs
@@ -11,6 +11,8 @@ namespace WhatsappNet.Api.Services.WhatsappCloud
         public WhasappCloudService()
         {
             _httpClient = new HttpClient();
+            var accesToken = "EAAKw08ZCoLhIBABzkVuxgLCMZBA1ZBpVw6GQo1XgOqnK0itjLNJihkOM37q0nBSqVFZBZCpFAgUfovzojuZC1BQKOwPelaI4XZANDf6lA0Aqp7wtj4X7BcoZBNraAirgio2ZAyO2IH92cAqkwdS4cBYovnLYjZBe4ByGBZBNavKuA6ESlxEYHyj0f5Ane9T6HHOS7tvcJc4Oui3qQZDZD";
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {accesToken}");
         }
 
         public async Task<bool> Execute(Object model)
@@ -19,11 +21,9 @@ namespace WhatsappNet.Api.Services.WhatsappCloud
 
             using var content = new ByteArrayContent(byteData);
             var endpoint = "https://graph.facebook.com";
-            var phoneNumberId = "112445371812093";
-            var accesToken = "EAAKw08ZCoLhIBABzkVuxgLCMZBA1ZBpVw6GQo1XgOqnK0itjLNJihkOM37q0nBSqVFZBZCpFAgUfovzojuZC1BQKOwPelaI4XZANDf6lA0Aqp7wtj4X7BcoZBNraAirgio2ZAyO2IH92cAqkwdS4cBYovnLYjZBe4ByGBZBNavKuA6ESlxEYHyj0f5Ane9T6HHOS7tvcJc4Oui3qQZDZD";
+            var phoneNumberId = "112445371812093";            
             var uri = $"{endpoint}/v16.0/{phoneNumberId}/messages";
-            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {accesToken}");
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");            
 
             var response = await _httpClient.PostAsync(uri, content);
 
